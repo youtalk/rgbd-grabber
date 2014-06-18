e55d6fcfe072f071f9829cc17d9add1652d80594
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2014-06-18 20:11:27 +0900

    implement ds325 calibration

--- include/rgbd/camera/DS325Calibrator.h
+++ include/rgbd/camera/DS325Calibrator.h
@@ -13,6 +13,32 @@
 
 namespace rgbd {
 
+class DS325Calibration {
+public:
+    DS325Calibration(const std::string& params);
+
+    ~DS325Calibration();
+
+    void calibrateColor(cv::Mat &source, cv::Mat &result);
+
+    void calibrateDepth(cv::Mat &source, cv::Mat &result);
+
+    void calibrateAmplitude(cv::Mat &source, cv::Mat &result);
+
+private:
+    cv::Size _csize;
+
+    cv::Mat cameraMatrix[2], distCoeffs[2];
+
+    cv::Mat R, T, R1, R2, P1, P2, Q, F;
+
+    cv::Mat _rectifyMaps[2][2];
+
+    cv::Rect validRoi[2];
+
+    void loadParameters(const std::string& params);
+};
+
 class DS325Calibrator: public DepthCamera {
 public:
     DS325Calibrator(std::shared_ptr<DS325> camera, const std::string& file);
@@ -38,7 +64,7 @@ public:
 private:
     std::shared_ptr<DS325> _camera;
 
-    cv::Mat _rectifyMaps[2];
+    DS325Calibration _calib;
 };
 
 }
--- src/camera/DS325Calibrator.cpp
+++ src/camera/DS325Calibrator.cpp
@@ -8,32 +8,93 @@
 
 namespace rgbd {
 
-DS325Calibrator::DS325Calibrator(std::shared_ptr<DS325> camera,
-                                   const std::string& file):
-        _camera(camera) {
-    cv::Mat cameraMatrix;
-    cv::Mat distCoeffs;
-    cv::FileStorage fs(file , CV_STORAGE_READ);
+DS325Calibration::DS325Calibration(const std::string& params) :
+        _csize(640, 480) {
+    loadParameters(params);
+}
+
+DS325Calibration::~DS325Calibration() {
+}
+
+void DS325Calibration::calibrateColor(cv::Mat& source, cv::Mat& result) {
+    cv::Mat temp;
+
+    cv::remap(source, temp, _rectifyMaps[0][0], _rectifyMaps[0][1], CV_INTER_LINEAR);
+    cv::resize(temp(validRoi[0]), result, _csize);
+}
+
+void DS325Calibration::calibrateDepth(cv::Mat& source, cv::Mat& result) {
+    const uint MAX_DEPTH = 1000;
+    const uint MIN_DEPTH = 0;
+
+    // I'm not sure why this is neccesary.
+    cv::Mat maxDist = cv::Mat::ones(_csize, CV_16U) * MAX_DEPTH;
+    cv::Mat minDist = cv::Mat::ones(_csize, CV_16U) * MIN_DEPTH;
+    cv::Mat scaled;
+    cv::resize(source, scaled, _csize);
+    cv::min(scaled, maxDist, scaled);
+    scaled -= minDist;
+
+    cv::Mat cropped = scaled(cv::Rect(40, 43, 498, 498 / 4 * 3)); // TODO
+    cv::resize(cropped, cropped, _csize);
+    cv::Mat temp;
+    cv::remap(cropped, temp, _rectifyMaps[1][0], _rectifyMaps[1][1], CV_INTER_LINEAR);
+    cv::resize(temp(validRoi[1]), result, _csize);
+}
+
+void DS325Calibration::calibrateAmplitude(cv::Mat& source, cv::Mat& result) {
+    cv::Mat scaled;
+    cv::resize(source, scaled, _csize);
+
+    cv::Mat cropped = scaled(cv::Rect(40, 43, 498, 498 / 4 * 3)); // TODO
+    cv::resize(cropped, cropped, _csize);
+    cv::Mat temp;
+    cv::remap(cropped, temp, _rectifyMaps[1][0], _rectifyMaps[1][1], CV_INTER_LINEAR);
+    cv::resize(temp(validRoi[1]), result, _csize);
+}
+
+void DS325Calibration::loadParameters(const std::string& params) {
+    cv::FileStorage fs(params.c_str(), CV_STORAGE_READ);
 
     if (fs.isOpened()) {
-        fs["cameraMatrix"] >> cameraMatrix;
-        fs["distCoeffs"] >> distCoeffs;
+        fs["M1"] >> cameraMatrix[0];
+        fs["D1"] >> distCoeffs[0];
+        fs["M2"] >> cameraMatrix[1];
+        fs["D2"] >> distCoeffs[1];
 
-        std::cerr << "DS325Calibrator: cameraMatrix = " << std::endl;
-        std::cout << cameraMatrix << std::endl;
-        std::cerr << "DS325Calibrator: distCoeffs = " << std::endl;
-        std::cout << distCoeffs << std::endl;
+        fs["R"] >> R;
+        fs["T"] >> T;
+        fs["R1"] >> R1;
+        fs["R2"] >> R2;
+        fs["P1"] >> P1;
+        fs["P2"] >> P2;
+        fs["Q"] >> Q;
+        cv::Mat_<int> vroiMat;
+        fs["vroi"] >> vroiMat;
 
-        cv::initUndistortRectifyMap(cameraMatrix, distCoeffs, cv::Mat(), cameraMatrix,
-                                    camera->colorSize(), CV_16SC2,
-                                    _rectifyMaps[0], _rectifyMaps[1]);
+        for (int i = 0; i < 2; ++i) {
+            validRoi[i].x = vroiMat.at<int>(i, 0);
+            validRoi[i].y = vroiMat.at<int>(i, 1);
+            validRoi[i].width = vroiMat.at<int>(i, 2);
+            validRoi[i].height = vroiMat.at<int>(i, 3);
+        }
 
-        std::cout << "DS325Calibrator: undistorted" << std::endl;
         fs.release();
     } else {
-        std::cerr << "DS325Calibrator: cannot open file" << std::endl;
+        std::cout << "Error: can not save the extrinsic parameters\n";
         std::exit(-1);
     }
+
+    cv::initUndistortRectifyMap(cameraMatrix[0], distCoeffs[0], R1, P1,
+                                _csize, CV_16SC2, _rectifyMaps[0][0], _rectifyMaps[0][1]);
+    cv::initUndistortRectifyMap(cameraMatrix[1], distCoeffs[1], R2, P2,
+                                _csize, CV_16SC2, _rectifyMaps[1][0], _rectifyMaps[1][1]);
+}
+
+DS325Calibrator::DS325Calibrator(std::shared_ptr<DS325> camera,
+                                   const std::string& file):
+        _camera(camera),
+        _calib(file) {
 }
 
 DS325Calibrator::~DS325Calibrator() {
@@ -49,7 +110,7 @@ void DS325Calibrator::start() {
 
 void DS325Calibrator::captureColor(cv::Mat& buffer) {
     _camera->captureColor(buffer);
-    cv::remap(buffer, buffer, _rectifyMaps[0], _rectifyMaps[1], CV_INTER_LINEAR);
+    _calib.calibrateColor(buffer, buffer);
 }
 
 void DS325Calibrator::captureRawColor(cv::Mat& buffer) {
@@ -57,6 +118,8 @@ void DS325Calibrator::captureRawColor(cv::Mat& buffer) {
 }
 
 void DS325Calibrator::captureDepth(cv::Mat& buffer) {
+    _camera->captureDepth(buffer);
+    _calib.calibrateDepth(buffer, buffer);
 }
 
 void DS325Calibrator::captureRawDepth(cv::Mat& buffer) {
@@ -64,6 +127,8 @@ void DS325Calibrator::captureRawDepth(cv::Mat& buffer) {
 }
 
 void DS325Calibrator::captureAmplitude(cv::Mat& buffer) {
+    _camera->captureAmplitude(buffer);
+    _calib.calibrateAmplitude(buffer, buffer);
 }
 
 void DS325Calibrator::captureRawAmplitude(cv::Mat& buffer) {
