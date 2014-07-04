55d5d492b2169e56bf79277cbc3989c5b856a496
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2014-07-04 11:35:42 +0900

    add DepthCameraCalibrator class

--- include/rgbd/camera/DS325Calibrator.h
+++ include/rgbd/camera/DS325Calibrator.h
@@ -8,7 +8,7 @@
 
 #include <iostream>
 #include <memory>
-#include "DepthCamera.h"
+#include "DepthCameraCalibrator.h"
 #include "DS325.h"
 
 namespace rgbd {
@@ -41,35 +41,21 @@ private:
     void loadParameters(const std::string& params);
 };
 
-class DS325Calibrator: public DepthCamera {
+class DS325Calibrator: public DepthCameraCalibrator {
 public:
     DS325Calibrator(std::shared_ptr<DS325> camera, const std::string& file);
 
     virtual ~DS325Calibrator();
 
-    virtual cv::Size colorSize() const;
-
-    virtual cv::Size depthSize() const;
-
-    virtual void start();
-
     virtual void captureColor(cv::Mat& buffer);
-
-    virtual void captureRawColor(cv::Mat& buffer);
 
     virtual void captureDepth(cv::Mat& buffer);
 
-    virtual void captureRawDepth(cv::Mat& buffer);
-
     virtual void captureAmplitude(cv::Mat& buffer);
-
-    virtual void captureRawAmplitude(cv::Mat& buffer);
 
     virtual void captureVertex(PointXYZVector& buffer);
 
 private:
-    std::shared_ptr<DS325> _camera;
-
     DS325Calibration _calib;
 };
 
---
+++ include/rgbd/camera/DepthCameraCalibrator.h
@@ -0,0 +1,35 @@
+/**
+ * @file DepthCameraCalibrator.h
+ * @author Yutaka Kondo <yutaka.kondo@youtalk.jp>
+ * @date Jul 4, 2014
+ */
+
+#pragma once
+
+#include "DepthCamera.h"
+
+namespace rgbd {
+
+class DepthCameraCalibrator: rgbd::DepthCamera {
+public:
+    DepthCameraCalibrator(std::shared_ptr<DepthCamera> camera);
+
+    virtual ~DepthCameraCalibrator();
+
+    virtual cv::Size colorSize() const;
+
+    virtual cv::Size depthSize() const;
+
+    virtual void start();
+
+    virtual void captureRawColor(cv::Mat& buffer);
+
+    virtual void captureRawDepth(cv::Mat& buffer);
+
+    virtual void captureRawAmplitude(cv::Mat& buffer);
+
+protected:
+    std::shared_ptr<DepthCamera> _camera;
+};
+
+}
